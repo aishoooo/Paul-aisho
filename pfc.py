5dbe112853cde1968ef44a57f4b80f4207987d79
aishooo <pchretien@guardiaschool.fr>
2022-11-02 11:37:53 +0100

    debut de code

---
+++ pfc.py
@@ -0,0 +1,15 @@
+class game:
+    def __init__(self,victory, element):
+    
+    self.element= pierre, feuille, ciseaux
+    self.victory= victory
+    self.choice= input('Choisissez entre "pierre", "feuille", "puit ou "ciseaux" !    ')
+    
+    def regle ()
+    
+class play:
+    
+
+    
+    
+class ia:
