7d1bf6be4526639226086edb715ee85e8effc1de
aishooo <pchretien@guardiaschool.fr>
2022-11-02 12:15:07 +0100

    creation de l ia

---
+++ ia.py
@@ -0,0 +1,10 @@
+class ia:
+    
+    def __init__(self):
+        self.stats = [pierre = 0.35, feuille = 0.30, ciseaux = 0.35]
+        self.ia_choice = 'pierre','feuille','ciseaux'
+        
+    def choice
+        if 
+    
+    
