c7e12ec3ec05cf5bdee40a9305b7553d0b7edc31
aishooo <pchretien@guardiaschool.fr>
2022-11-02 14:55:04 +0100

    essayer le dico

--- ia.py
+++ ia.py
@@ -1,10 +1,14 @@
 class ia:
     
-    def __init__(self):
-        self.stats = [pierre = 0.35, feuille = 0.30, ciseaux = 0.35]
+    def __init__(self, stats, ia_choice):
+        #self.stats = [pierre = 0.35, feuille = 0.30, ciseaux = 0.35]
+        self.stats = {"pierre" : 0.35,
+                      "feuille" : 0.30,
+                      "ciseaux" : 0.35, 
+                      }
         self.ia_choice = 'pierre','feuille','ciseaux'
-        
-    def choice
-        if 
+      
     
+    def choice():
+        return min(self.stats)
     
