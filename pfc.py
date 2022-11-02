88886af2b214220b6d4ebcc4878876c89ea81b62
aishooo <pchretien@guardiaschool.fr>
2022-11-02 11:52:54 +0100

    debut des regles

--- pfc.py
+++ pfc.py
@@ -1,15 +1,32 @@
 class game:
     def __init__(self,victory, element):
     
-    self.element= pierre, feuille, ciseaux
-    self.victory= victory
-    self.choice= input('Choisissez entre "pierre", "feuille", "puit ou "ciseaux" !    ')
+        self.element= 'pierre', 'feuille', 'ciseaux'
+        self.victory= victory
+        self.user_choice= input('Choisissez entre "pierre", "feuille", "puit ou "ciseaux" !    ')
     
-    def regle ()
     
+   
+    def regle():
+        if(user_choice == 'pierre' and ia == 'feuille'):
+            print('ta gagner, tu es devenu beau!')
+        elif(user_choice == 'feuille' and ia == 'pierre'):
+            print('ahaha tu as perdu !')
+        elif(user_choice == 'ciseaux' and ia == 'pierre'):
+              print('ahaha tu as perdu !')
+        elif(user_choice== 'pierre' and ia == 'ciseaux'):
+            print('ta gagner, tu es devenu beau')
+        elif(user_choice == 'ciseaux' and ia == 'feuille'):
+            print('ta gagner, tu es devenu beau')
+        elif(user_choice== 'feuille' and ia == 'ciseaux'):
+            print('ahaha tu as perdu !')
+        
+    if 
 class play:
     
-
+ while self.choice!= self.element
+    self.user_choice = input('Choisissez entre "pierre", "feuille", "puit ou "ciseaux" !    ')
+    
     
     
 class ia:
