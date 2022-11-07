9b323a770f26cef221f93e8e813bac2dfc2ed980
aishooo <pchretien@guardiaschool.fr>
2022-11-07 16:20:45 +0100

    regle, compteur et le choix des joueur plus easter eggs

---
+++ game.py
@@ -0,0 +1,71 @@
+import random
+
+class Game:
+    def __init__(self, player1, player2):
+    
+        self.compteur = 0
+        self.player1= player1
+        self.player2= player2
+        self.compteur_player1=0
+        self.compteur_player2=0
+    
+    def play(self):
+        while self.end() == False :
+            self.who_win()
+            
+        
+    def who_win(self) :
+        if self.player1.nom in ['Crocodile', 'dreaM4ker', 'Aisho66']:
+            return print( "l'ia : je declare forfait devant tant de pouissance, créateur vous etes trop forts")    
+        else:
+            choice_1 = self.player1.choice()
+            choice_2= self.player2.choice()
+        
+    
+        if(choice_1 == 'feuille' and choice_2 == 'pierre'):
+            self.compteur_player1 +=1
+            print(f"le joueur {self.player1.nom} a gagne \n" )
+
+        elif(choice_1== 'pierre' and choice_2 == 'ciseaux'):
+            self.compteur_player1 +=1
+            print(f"le joueur {self.player1.nom} a gagne, score:  {self.compteur_player1} - {self.compteur_player2}\n" )
+            
+
+        elif(choice_1 == 'ciseaux' and choice_2 == 'feuille'):
+            self.compteur_player1 +=1
+            print(f"le joueur {self.player1.nom} a gagne , score:  {self.compteur_player1} - {self.compteur_player2}\n" )
+
+        elif(choice_1 == 'pierre' and choice_2 == 'feuille'):
+            self.compteur_player2 +=1
+            print(f"le joueur {self.player2.nom} a gagne, score:  {self.compteur_player1} - {self.compteur_player2}\n" )
+
+        elif(choice_1 == 'ciseaux' and choice_2 == 'pierre'):
+            self.compteur_player_2 +=1
+            print(f"le joueur {self.player2.nom} a gagne , score:  {self.compteur_player1} - {self.compteur_player2}\n" )
+        
+        elif(choice_1== 'feuille' and choice_2 == 'ciseaux'):
+            self.compteur_player_2 +=1
+            print(f"le joueur {self.player2.nom} a gagne , score:  {self.compteur_player1} - {self.compteur_player2}\n" )
+            
+        elif(choice_1 == 'pierre' and choice_2 == 'pierre'):
+            print("egalite \n")
+        
+        elif(choice_1 == 'ciseaux' and choice_2 == 'ciseaux'):
+            print("egalite \n")
+        
+        elif(choice_1 == 'feuille' and choice_2 == 'feuille'):
+            print("egalite \n")
+        
+            
+            
+        
+    def end(self) :
+        if self.compteur < 3:
+            return False
+        else :
+            return True
+        
+    
+    def __compteur(self):
+        self.compteur_player1=0
+        self.compteur_player2=0
