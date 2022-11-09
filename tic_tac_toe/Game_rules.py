4231fb493b3558c03245e5a53c5eb5abd3a69174
aishoooo <117069257+aishoooo@users.noreply.github.com>
2022-11-09 18:06:04 +0100

    Update Game_rules.py
    reconfiguration des règles du a un mauvais placement des victoire

--- tic_tac_toe/Game_rules.py
+++ tic_tac_toe/Game_rules.py
@@ -39,13 +39,13 @@ class morpion:
         if (self.board[0][0] == player.symbol and self.board[1][0] == player.symbol  and self.board[2][0] == player.symbol ) :
             print (player.nom + " a gagné")
             return(True)
-        elif (self.board[0][1]== player.symbol and self.board[1][1] == player.symbol  and self.board[2][0] == player.symbol ):
+        elif (self.board[0][1]== player.symbol and self.board[1][1] == player.symbol  and self.board[2][1] == player.symbol ):
             print (player.nom + " a gagné")
             return (True)
         elif (self.board[0][2]== player.symbol and self.board[1][2] == player.symbol  and self.board[2][2] == player.symbol ):
             print (player.nom + " a gagné")
             return (True)
-        elif (self.board[0][0]== player.symbol and self.board[1][1] == player.symbol  and self.board[1][2] == player.symbol ):
+        elif (self.board[0][0]== player.symbol and self.board[0][1] == player.symbol  and self.board[0][2] == player.symbol ):
             print (player.nom + " a gagné")
             return (True)
         elif (self.board[1][0]== player.symbol and self.board[1][1] == player.symbol  and self.board[1][2] == player.symbol ):
@@ -57,7 +57,7 @@ class morpion:
         elif (self.board[0][0]== player.symbol and self.board[1][1] == player.symbol  and self.board[2][2] == player.symbol ):
             print (player.nom + " a gagné")
             return (True)
-        elif (self.board[0][1]== player.symbol and self.board[0][0] == player.symbol  and self.board[0][2] == player.symbol ):
+        elif (self.board[2][0]== player.symbol and self.board[1][1] == player.symbol  and self.board[0][2] == player.symbol ):
             print (player.nom + " a gagné")
             return (True)
         elif ' ' not in (self.board_front[0]+self.board_front[1]+self.board_front[2]):
@@ -69,10 +69,4 @@ class morpion:
 player_1 = Player('X')
 player_2 = Player('O')
 a = morpion(player_1, player_2)
-#for i in a.board_front:
-#    print(i)
-#a.player2.choice_player()
-#for i in a.board_front:
-#    print(i)
-
 a.play()
