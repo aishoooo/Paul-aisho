c9a2117f3661740fa4385e085151e9243299072d
aishooo <pchretien@guardiaschool.fr>
2022-11-09 12:02:26 +0100

    player

--- tic_tac_toe/class_Player.py
+++ tic_tac_toe/class_Player.py
@@ -1,13 +1,27 @@
 class Player:
-
-    def __init__(self):
+    def __init__(self, symbol):
+        self.points = 0
+        self.symbol = symbol
         self.nom = input('Entrez le nom de votre joueur : ')
-
-    def choice_player(self):
-        ligne= int(input('choississez la ligne : ')) 
-        colone= int(input('choississez la colonne : ')) 
-        return ligne, colone
+        while self.nom == "":
+            print("Veuiller mettre un nom")
+            self.nom = input('Entrez le nom de votre joueur : ')
         
+
+    
+    def choice_player(self):
+        line= input('choississez la line : ')
+        column= input('choississez la column : ')
+        
+        valid_list = ["0", "1", "2"]
+        
+        while line not in valid_list or column not in valid_list:
+            print("Choississez entre 0 et 2")
+            line= input('choississez la line : ') 
+            column= input('choississez la column : ')  
+        return int(line), int(column)
+       
+    
         
         
         
