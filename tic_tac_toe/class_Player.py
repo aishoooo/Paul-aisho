8ae805b7c19c65e7b9661aa2f5f18a2e7e40fcf6
aishoooo <117069257+aishoooo@users.noreply.github.com>
2022-11-10 07:21:54 +0100

    Update class_Player.py
    réactualisation de la class Player

--- tic_tac_toe/class_Player.py
+++ tic_tac_toe/class_Player.py
@@ -1,28 +1,24 @@
 class Player:
-    def __init__(self, symbol):
+    def init(self, symbol):
         self.points = 0
         self.symbol = symbol
-        self.nom = input('Entrez le nom de votre joueur : ')
-        while self.nom == "":
+        self.name = input('Entrez le nom de votre joueur : ')
+        while self.name == "":
             print("Veuiller mettre un nom")
-            self.nom = input('Entrez le nom de votre joueur : ')
-        
+            self.name = input('Entrez le nom de votre joueur : ')
 
-    
+
+
     def choice_player(self):
-        line= input('choississez la line : ')
-        column= input('choississez la column : ')
-        
+        line= input('choississez la ligne(entre 0 et 2) : ')
+        column= input('choississez la colonne(entre 0 et 2): ')
+
         valid_list = ["0", "1", "2"]
-        
+
         while line not in valid_list or column not in valid_list:
-            print("Choississez entre 0 et 2")
-            line= input('choississez la line : ') 
-            column= input('choississez la column : ')  
+            print("Vous vous êtes trompés choisissez bien un chiffre entre 0 et 2")
+            line= input('choississez la ligne : ') 
+            column= input('choississez la colonne : ')
         return int(line), int(column)
-       
-    
-        
-        
         
         
